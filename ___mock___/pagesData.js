57857809a5832689432e080c674902d2583648c4
LGers <87089817+LGers@users.noreply.github.com>
2022-08-11 10:56:31 +0300

    feat: base app

---
+++ ___mock___/pagesData.js
@@ -0,0 +1,5 @@
+export const mainPageData = {
+  title: 'Used trucks for sale, best prices for vehicles at Truck1',
+  keywords: 'trucks, sale, buy, sell, prices, dealers, used, new, marketplace',
+  description: 'Browse 150 000 offers of trucks for sale in Europe. Buy new and used trucks, trailers, vans and machinery in one place for fair prices at Truck1',
+}
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Wrapper } from '../components/Wrapper';
+import { mainPageData } from '../___mock___/pagesData';
+
+const Index = () => {
+
+  return (
+    <Wrapper {...mainPageData}>
+        <h1>Truck1.eu - Main page</h1>
+    </Wrapper>
+  );
+};
+
+export default Index;
